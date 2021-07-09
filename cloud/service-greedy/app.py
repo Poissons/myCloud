281f01eca50d23658916dab35441c855dc4ead4c
qiaoyufei <17822000907@163.com>
2021-07-09 16:08:56 +0800

    Update app.py

--- cloud/service-greedy/app.py
+++ cloud/service-greedy/app.py
@@ -3,7 +3,7 @@
 # 	python app.py
 # Submit a request via cURL:
 
-# curl http://localhost:4001/predict -X POST -d 'observation=["master", 3, {"master": {"4": {"success": 1, "failure": 0}, "11": {"success": 0, "failure": 1}, "8": {"success": 1, "failure": 0}}}, {"master": {"node1": {"4": 2, "11": 1}, "node2": {"8": 2}}}, {"master": {"4": {"stuck": 1}, "11": {"stuck": 1}, "8": {"stuck": 1}, "6": {"stuck": 1}, "2": {"stuck": 1}, "1": {"stuck": 2}}}, {"master": {'node1':{'memory':{'percent':'13','number':'2098Mi'},'storage':{'percent':'3','number':'4Gi'},'cpu':{'percent':'2','number':'100m'}},'node2':{'memory':{'percent':'13','number':'2098Mi'},'storage':{'percent':'3','number':'4Gi'},'cpu':{'percent':'2','number':'100m'}}}}, 2]'
+# curl http://localhost:4001/predict -X POST -d 'observation=["master", 3, {"master": {"4": {"success": 1, "failure": 0}, "11": {"success": 0, "failure": 1}, "8": {"success": 1, "failure": 0}}}, {"master": {"node1": {"4": 2, "11": 1}, "node2": {"8": 2}}}, {"master": {"4": {"stuck": 1}, "11": {"stuck": 1}, "8": {"stuck": 1}, "6": {"stuck": 1}, "2": {"stuck": 1}, "1": {"stuck": 2}}}, {"master": {"node1":{"memory":{"percent":"13","number":"2098Mi"},"storage":{"percent":"3","number":"4Gi"},"cpu":{"percent":"2","number":"100m"}},"node2":{"memory":{"percent":"13","number":"2098Mi"},"storage":{"percent":"3","number":"4Gi"},"cpu":{"percent":"2","number":"100m"}}}}, 0]'
 # tasks_execute_situation_on_each_node_dict, current_service_on_each_node_dict,
 # stuck_tasks_situation_on_each_node_dict, resources_on_each_node_dict, epoch_index]'
 
