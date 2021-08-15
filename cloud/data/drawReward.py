a86f78c283c756f928b76d14fe2132b74de1078e
qiaoyufei <17822000907@163.com>
2021-08-15 21:35:46 +0800

    collect

--- cloud/data/drawReward.py
+++ cloud/data/drawReward.py
@@ -28,8 +28,8 @@ if __name__ == "__main__":
     plt.title('reward', fontsize=20)
     plt.ylabel(u'成功率', fontsize=10)
 
-    plt.plot(dqn_data['epoch'], dqn_data['reward_percent'], color='deeppink', linewidth=1.5, label='dqn')
-    plt.plot(greedy_data['epoch'], greedy_data['reward_percent'], color='darkblue', linewidth=1.5, label='failure')
+    plt.plot(dqn_data['epoch'], dqn_data['reward_percent'], color='darkblue', linewidth=1.5, label='dqn')
+    plt.plot(greedy_data['epoch'], greedy_data['reward_percent'], color='deeppink', linewidth=1.5, label='greedy')
 
     plt.legend(loc=2)
     plt.savefig('./10_21_6/reward.png')
